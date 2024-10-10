2f6b9e777fbd0b7e293035f0dc7a54cfcd420bf0
fimaAidarous <fma0899@gmail.com>
2024-10-09 22:27:29 -0700

    getsupplierById

--- api/controllers/supplierController.js
+++ api/controllers/supplierController.js
@@ -1,4 +1,5 @@
 import Supplier  from '../models/supplierModel.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createSupplier = async (req, res, next) => {
     const { name, contact } = req.body;
@@ -27,3 +28,16 @@ export const getAllsuppliers = async (req,res,next) => {
   }
 };
 
+
+// Get a supplier by ID
+export const getSupplierById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const supplier = await Supplier.findById(id);
+    if (!supplier) return next(errorHandler(404, 'Supplier not found'));
+    res.status(200).json(supplier);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/supplierRoute.js
+++ api/routes/supplierRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createSupplier, getAllsuppliers,} from '../controllers/supplierController.js';
+import { createSupplier, getAllsuppliers,getSupplierById} from '../controllers/supplierController.js';
 
 const router = express.Router();
 
 router.post('/', createSupplier);
 router.get('/', getAllsuppliers);
+router.get('/:id', getSupplierById);
 
 export default router;
